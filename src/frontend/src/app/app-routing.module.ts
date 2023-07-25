814291f2efdcd1ce628c39eb74852d37145eb3eb
kkasper1 <01159308@pw.edu.pl>
2023-07-25 18:47:59 +0200

    basic frontend

--- src/frontend/src/app/app-routing.module.ts
+++ src/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
+import { HtmlParser } from '@angular/compiler';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { MyAccountComponent } from './my-account/my-account.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'main',
+    component: MainPageComponent
+  },
+  {
+    path: 'myaccount',
+    component: MyAccountComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
