31edd44580febbd31bdd85686544be87b92c4a81
sdyszews <01158617@pw.edu.pl>
2023-09-27 17:53:24 +0200

    frontend backet integration

--- src/frontend/src/app/basket/basket.component.ts
+++ src/frontend/src/app/basket/basket.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
+import { concatMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-basket',
@@ -6,5 +10,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./basket.component.css']
 })
 export class BasketComponent {
+  constructor(private router: Router, private http: HttpClient, private cookieservice: CookieService) {}
 
+  id: string = "";
+
+  name: string[] = [];
+  price: string[] = [];
+  
+  ngOnInit() {
+    const client = {
+      email: this.cookieservice.get('SESSION_TOKEN')
+    };
+
+    this.http.post('http://localhost:8082/getBasketOfClient', client).subscribe(
+      (response: any) => {
+        if (response != null)
+        {
+          this.id = response.id;
+          const order = {
+            order_id: response.id
+          };
+          this.http.post('http://localhost:8082/getProductsFromOrder', order).subscribe(
+            (response: any) => {
+              for (const value of response) {
+                this.name.push(value.name);
+                this.price.push(value.price);
+              }
+            },
+            (error) => {
+              console.error('Błąd podczas pobierania danych', error);
+            }
+          );
+        }
+        else
+        {
+          console.log('Koszyk jest pusty');
+        }
+      },
+      (error) => {
+        console.error('Błąd podczas pobierania danych', error);
+      }
+    );
+  }
 }
